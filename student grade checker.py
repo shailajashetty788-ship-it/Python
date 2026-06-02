bad5a052e62d21b8198885ae4429f5fdf5180b97
shailajashetty788-ship-it <shailajashetty788@gmail.com>
2026-06-02 15:13:55 +0530

    Add files via upload

---
+++ student grade checker.py
@@ -0,0 +1,15 @@
+a=int(input("enter the marks:"))
+if a>90:
+	                print("a+:")
+elif a>80:
+	                    print("a:")
+elif  a>75:
+	                    print("b+:")
+elif a>60:
+                       print("b:")
+elif a>50:
+                       print("c+:")
+elif  a>40:
+	                    print("c:")
+else:
+	    print("f:")
